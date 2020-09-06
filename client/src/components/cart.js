5a721ab2fbb631e1ec1ef0d10a8dac74d3437792
JOSH-PC\Josh <josh.campbell4000@gmail.com>
2020-09-06 22:54:08 +0930

    added kounta dummy order data

--- client/src/components/cart.js
+++ client/src/components/cart.js
@@ -65,7 +65,49 @@ class CartClass extends Component {
 
     // submitCartOrder() - SUBMITS CART TO KOUNTA ON SUCCESS OF STRIPE
     submitCartOrder = function(){
-        
+        let dummyData = {
+            "status": "ON_HOLD",
+            "notes": "Take away, gold member",
+            "order_type": "Dine in",
+            "guests": 23,
+            "customer_id": 3842,
+            "site_id": 409,
+            "lines": [
+              {
+                "product_id": 730297,
+                "quantity": 6,
+                "notes": "Extra hot",
+                "unit_price": 1.3636
+              },
+              {
+                "product_id": 5312,
+                "quantity": 1,
+                "price_variation": 0.9,
+                "modifiers": [
+                  228,
+                  228,
+                  -3091
+                ]
+              }
+            ],
+            "price_limit": 100,
+            "price_variation": 1.15,
+            "payments": [
+              {
+                "method_id": 1,
+                "amount": 5.5,
+                "ref": "9000768"
+              }
+            ],
+            "callback_uri": "http://third-party.com/update_order_status.json?id=123582936748",
+            "complete_when_paid": false,
+            "pass_thru_printing": false,
+            "lock": [
+              "PAYMENTS"
+            ],
+            "placed_at": "2013-05-29T11:14:06+10:00",
+            "fulfil_at": "2013-05-29T13:00:00+10:00"
+          }
     }
 }
 
